deaebf151f5e59e9712cd12797e70100195b81a6
ben wozak <benwozak@bens-MacBook-Pro-3.local>
2024-12-12 13:19:17 -0700

    feat!: Migrated project from JavaScript to TypeScript and added package dependancies
    BREAKING CHANGE: Project now requires TypeScript.

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,41 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import globals from 'globals'
 
 export default [
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
-  },
-
-  {
-    name: 'app/files-to-ignore',
+    files: ['**/*.{js,mjs,jsx,ts,tsx,vue}'],
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
+
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: pluginVue.parser,
+      parserOptions: {
+        parser: tsParser,
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+        extraFileExtensions: ['.vue'],
+      },
+    },
+    plugins: {
+      vue: pluginVue,
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs['recommended'].rules,
+      'vue/script-setup-uses-vars': 'error',
+    },
+    globals: {
+      ...globals.browser,
+      ...globals.es2021,
+    },
+  },
 ]
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
-<script setup>
-
+<script setup lang="ts">
+import Button from './components/ui/button/Button.vue';
 </script>
 
 <template>
   <main>
-    ...entrypoint
+    <Button>Test</Button>
   </main>
 </template>
 
